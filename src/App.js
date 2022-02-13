85ac6a327ef868fbd36b30bcea84eca7b8ea2f4c
Cristian <cristian.andrisen@gmail.com>
2022-02-13 19:20:10 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -6,51 +6,63 @@ import apiViaCep from './services/api.js';
 
 function App() {
 
-  const [cep, setCep] = useState('');
+    const [cep, setCep] = useState('');
 
-  const [dadosApiCep, setDadosApiCep] = useState({});
+    const [dadosApiCep, setDadosApiCep] = useState({});
 
-  async function SearchCep() {
-    if (cep === '') {
-      alert('Preencha algum cep para pesquisar!');
-      return;
+    async function SearchCep() {
+        if (cep === '') {
+            alert('Preencha algum cep para pesquisar!');
+            return;
+        }
+
+        try {
+            const response = await apiViaCep.get(cep + '/json');
+            setDadosApiCep(response.data);
+        } catch {
+            alert('Ocorreu um erro ao buscar o cep...');
+        }
+
     }
 
-    try {
-      const response = await apiViaCep.get(cep + '/json');
-      setDadosApiCep(response.data);
-    } catch {
-      alert('Ocorreu um erro ao buscar o cep...');
+
+    return ( <
+        div className = "app" >
+        <
+        div className = "container" >
+        <
+        h1 className = "title" > Busca CEP < /h1> <
+        div className = "div-inputs" >
+        <
+        input type = "text"
+        placeholder = "Digite aqui um cep..."
+        value = { cep }
+        onChange = {
+            (e) => setCep(e.target.value)
+        }
+        /> <
+        button className = 'btn-search'
+        onClick = { SearchCep } > < FiSearch size = { 15 }
+        /></button >
+        <
+        /div> {
+        Object.keys(dadosApiCep).length > 0 && ( <
+            div className = 'data-adress' >
+            <
+            span > { dadosApiCep.bairro } < /span> <
+            span > { dadosApiCep.logradouro } < /span> <
+            span > { dadosApiCep.localidade }, { dadosApiCep.uf } < /span> < /
+            div >
+        )
     }
 
-  }
 
-  return (
-    <div className="app">
-      <div className="container">
-        <h1 className="title">Busca CEP</h1>
-        <div className="div-inputs">
-          <input
-            type="text"
-            placeholder="Digite aqui um cep..."
-            value={cep}
-            onChange={(e) => setCep(e.target.value)}
-          />
-          <button className='btn-search' onClick={SearchCep}><FiSearch size={15} /></button>
-        </div>
-        {Object.keys(dadosApiCep).length > 0 && (
-          <div className='data-adress'>
-            <span> {dadosApiCep.bairro} </span>
-            <span> {dadosApiCep.logradouro} </span>
-            <span> {dadosApiCep.localidade}, {dadosApiCep.uf} </span>
-          </div>
-        )}
+    <
+    /div>
 
-
-      </div>
-
-    </div>
-  );
+    <
+    /div>
+);
 }
 
 export default App;
